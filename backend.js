c1bfaea1356c788f08173dd666f297122a26ded9
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-16 16:59:30 +0100

    new changes

--- backend.js
+++ backend.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import mongodb, { MongoClient } from 'mongodb';
-import cors from 'cors';
+import express from "express";
+import mongodb, { MongoClient } from "mongodb";
+import cors from "cors";
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,66 +9,68 @@ const app = express();
 // const port = process.env.PORT || 3016;
 const port = 3016;
 // const mongoConnectString = process.env.MONGODB_URI;
-const mongoConnectString = 'mongodb://localhost:27017';
+const mongoConnectString = "mongodb://localhost:27017";
 const client = new MongoClient(mongoConnectString);
 
 app.use(cors());
 app.use(express.json());
 
 const execMongo = async (done) => {
-	await client.connect();
-	const db = client.db('users100');
-	done(db);
+  await client.connect();
+  const db = client.db("api001");
+  done(db);
 };
 
-app.get('/', (req, res) => {
-	execMongo(async (db) => {
-		const users = await db
-			.collection('usersdata')
-			.find()
-			.project({
-				name: 1,
-				username: 1,
-				email: 1
-			})
-			.toArray();
-		res.json(users);
-	});
+app.get("/", (req, res) => {
+  execMongo(async (db) => {
+    const users = await db
+      .collection("users100")
+      .find()
+      .project({
+        name: 1,
+        username: 1,
+        email: 1,
+      })
+      .toArray();
+    res.json(users);
+  });
 });
 
-app.delete('/deleteuser/:id', (req, res) => {
-	const id = req.params.id;
-	execMongo(async (db) => {
-		const deleteResult = await db.collection('usersdata').deleteOne({ _id: new mongodb.ObjectId(id) });
-		res.json({
-			result: deleteResult
-		});
-	});
+app.delete("/deleteuser/:id", (req, res) => {
+  const id = req.params.id;
+  execMongo(async (db) => {
+    const deleteResult = await db
+      .collection("users100")
+      .deleteOne({ _id: new mongodb.ObjectId(id) });
+    res.json({
+      result: deleteResult,
+    });
+  });
 });
 
-app.post('/adduser', (req, res) => {
-	const user = req.body.user;
-	execMongo(async (db) => {
-		const insertResult = await db.collection('usersdata').insertOne(user);
-		res.json({
-			result: insertResult
-		});
-	});
+app.post("/adduser", (req, res) => {
+  const user = req.body.user;
+  execMongo(async (db) => {
+    const insertResult = await db.collection("users100").insertOne(user);
+    res.json({
+      result: insertResult,
+    });
+  });
 });
 
-app.patch('/edituser/:id', (req, res) => {
-	const id = req.params.id;
-	const email = req.body.email;
-	execMongo(async (db) => {
-		const updateResult = await db
-			.collection('users100')
-			.updateOne({ _id: new mongodb.ObjectId(id) }, { $set: { email } });
-		res.json({
-			result: updateResult
-		});
-	});
+app.patch("/edituser/:id", (req, res) => {
+  const id = req.params.id;
+  const email = req.body.email;
+  execMongo(async (db) => {
+    const updateResult = await 
+    db.collection("users100")
+      .updateOne({ _id: new mongodb.ObjectId(id) }, { $set: { email } });
+    res.json({
+      result: updateResult,
+    });
+  });
 });
 
 app.listen(port, () => {
-	console.log(`listening on http://localhost:${port}`);
+  console.log(`listening on http://localhost:${port}`);
 });
