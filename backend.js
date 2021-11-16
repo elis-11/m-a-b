6345ab5ad35d7b4ce7d822410b97e88764e2b5e4
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-16 15:57:05 +0100

    fix doc

--- backend.js
+++ backend.js
@@ -6,10 +6,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3016;
-// const port = 3016;
-const mongoConnectString = process.env.MONGODB_URI;
-// const mongoConnectString = "mongodb://localhost:27017";
+// const port = process.env.PORT || 3016;
+const port = 3016;
+// const mongoConnectString = process.env.MONGODB_URI;
+const mongoConnectString = "mongodb://localhost:27017";
 const client = new MongoClient(mongoConnectString);
 
 app.use(cors());
