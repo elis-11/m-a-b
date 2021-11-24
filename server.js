9d6294b5c51f225543d79e0e1c8dc1aeac257e9b
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-24 13:51:10 +0100

    DELETE function

--- server.js
+++ server.js
@@ -34,6 +34,15 @@ app.patch('/simpleUsers/update/:id', async (req, res) => {
     });
 });
 
+// simple users: DELETE
+app.delete('/simpleUsers/delete/:id', async (req, res) => {
+    const id = req.params.id;
+    const result = await SimpleUsersController.deleteSimpleUser(id);
+    res.json({
+        result
+    });
+});
+
 app.listen(port, () => {
     console.log(`API is now listening on port ${port}`);
 });
