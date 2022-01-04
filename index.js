b0d9c3592f2a7ff6d3b578cf915f8ce48d679188
elis-11 <eliza.arzanukaeva@gmail.com>
2022-01-04 16:00:15 +0100

    routers, INSOMNIA, send

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 
 //set up server
 
@@ -10,13 +10,20 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(` :) Server started on port ${PORT}`));
 
+app.use(express.json());
+
 // connect to mongoDB
 
-mongoose.connect(process.env.MDB_CONNECT,
-//     {useNewUrlParser: true,
-//    iseUnifiedTopology: true,},
-   
- (err) =>{
-    if (err) return console.error(err)
-    console.log('Connected to MongoDB');
-})
+mongoose.connect(
+  process.env.MDB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+
+  (err) => {
+    if (err) return console.error(err);
+    console.log("Connected to MongoDB");
+  }
+);
+
+// set up routes
+
+app.use("/auth", require("./routers/userRouter"));
--- notes.md
+++ notes.md
@@ -3,7 +3,7 @@
 - start: 'node index' / npm start
 - dev: 'nodemon index' / npm run dev
 - 12:30 INSOMNIA
--------------------
+--------------------
 app.get("/test", (req, res) => {
     res.send("It works")
 })
@@ -19,10 +19,12 @@ app.get("/test", (req, res) => {
 - model schema
 ------------
 ## 4:
-- 
+- ROUTERS
+- INSOMNIA
+- 3:50 insomnia send
 
 
 
 
-
+-------------------------------
 ## https://www.youtube.com/watch?v=scYojqjnHzI&list=PLJM1tXwlGdaf57oUx0rIqSW668Rpo_7oU
