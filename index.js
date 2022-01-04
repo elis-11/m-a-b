ec12411819aa422f4ba0b61e020f8ce112a4a6fa
elis-11 <eliza.arzanukaeva@gmail.com>
2022-01-04 14:22:00 +0100

    .env, connecting to MongoDB, i mongoose & dotenv

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config()
+
+//set up server
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(` :) Server started on port ${PORT}`));
 
-app.get("/test", (req, res) => {
-    res.send("It works")
+// connect to mongoDB
+
+mongoose.connect(process.env.MDB_CONNECT,
+//     {useNewUrlParser: true,
+//    iseUnifiedTopology: true,},
+   
+ (err) =>{
+    if (err) return console.error(err)
+    console.log('Connected to MongoDB');
 })
+
--- notes.md
+++ notes.md
@@ -3,3 +3,19 @@
 - start: 'node index' / npm start
 - dev: 'nodemon index' / npm run dev
 - 12:30 INSOMNIA
+---------------------
+app.get("/test", (req, res) => {
+    res.send("It works")
+})
+---------------------
+## 2:
+- ATLAS
+- .env
+- connecting to MongoDB
+- npm i mongoose dotenv
+
+
+
+
+
+## https://www.youtube.com/watch?v=scYojqjnHzI&list=PLJM1tXwlGdaf57oUx0rIqSW668Rpo_7oU
