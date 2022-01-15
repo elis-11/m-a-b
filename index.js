760c7af165a33a41d7cb7045cc33d9ffafccb116
elis-11 <eliza.arzanukaeva@gmail.com>
2022-01-15 21:56:26 +0100

    mongoose@5

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 dotenv.config();
+// connect to mongoDB
+mongoose.connect(
+  process.env.MDB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+
+  (err) => {
+    if (err) return console.error(err);
+    console.log("Connected to MongoDB");
+  }
+);
 
 //set up server
 
@@ -16,23 +26,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: "http://localhost:3000",
   })
 );
-
-// connect to mongoDB
-
-mongoose.connect(
-  process.env.MDB_CONNECT,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-
-  (err) => {
-    if (err) return console.error(err);
-    console.log("Connected to MongoDB");
-  }
-);
-
 // set up routes 5:17
-
 app.use("/auth", require("./routers/userRouter"));
 app.use("/customer", require("./routers/customerRouter"));
